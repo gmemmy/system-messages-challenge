3bef907862cc1ffad028c14db7a65e44f12f1b2b
gmemmy <gmemmy22@gmail.com>
2019-11-17 11:39:09 +0100

    first commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+const cardDetails = [
+  {
+    title: 'Warning',
+    content: 'Your device storage is almost full. If \n you choose to proceed, you might \n have to decide some games.',
+    backgroundColor: '#fd7367',
+    buttonText: 'I understand'
+  },
+  {
+    title: 'Update error',
+    content: 'The internet connection was \n interrupted. The receiver will not \n receive a notification. Please try again.',
+    backgroundColor: '#d40b68',
+    buttonText: 'Try again'
+  },
+  {
+    title: 'Update successful!',
+    content: 'Your shipment records have been \n uploaded and the receiver has been \n sent a notification. Nice!',
+    backgroundColor: '#41d58b',
+    buttonText: 'Ok'
+  },
+]
+
+export default cardDetails;
